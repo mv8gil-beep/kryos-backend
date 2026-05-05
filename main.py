b907965851803280d6900b05bc92238e5398210e
Michael J. Vigil <michaelj.vigil@iMac.lan>
2026-05-05 14:04:30 -0700

    enable cors

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import uuid
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 paid_reports = {}
 
